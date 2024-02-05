ebeefc38ad74977561be280192c899119b886400
Bhaskara Ida Bagus <gusbhas@gmail.com>
2024-02-05 08:04:59 +0000

    update constants

--- src/reusable/constants.js
+++ src/reusable/constants.js
@@ -2,10 +2,20 @@ import { Helmet } from "react-helmet"
 
 export const apiUrl = 'https://backend-balisanti.siwalatri.klungkungkab.go.id/api/'
 
+// export const apiUrl = 'http://192.168.0.199:8888/api/'
+
 export function helmetAppender(head){
-    return(
-        <Helmet>
-            <title>{head} | Bali Santi</title>
-        </Helmet>
-    )
+    if (head.includes("Bali Santi")) {
+        return(
+            <Helmet>
+                <title>{head}</title>
+            </Helmet>
+        )
+    } else {
+        return(
+            <Helmet>
+                <title>{head} | Bali Santi</title>
+            </Helmet>
+        )
+    }
 }
