c668e5d57877615200ff21210f2c11adb3ec55c6
Bhaskara Ida Bagus <gusbhas@gmail.com>
2024-08-25 19:18:00 +0000

    add kontak kami in footer

--- src/containers/Footer.js
+++ src/containers/Footer.js
@@ -3,98 +3,73 @@ import { Navbar, Nav, Container } from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "../../src/assets/css/style.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faApple, faGooglePlay, faTiktok } from '@fortawesome/free-brands-svg-icons';
+import { faEnvelope, faGlobe } from "@fortawesome/free-solid-svg-icons";
+import { faTiktok, faFacebook, faLinkedin, faWhatsapp, faInstagram, faYoutube } from "@fortawesome/free-brands-svg-icons";
+
 import logoWhite from "./../assets/atixsiwalatri.png";
 import axios from "axios";
 
 const Footer = () => {
 
-//     const [datas, setData] = useState([])
-
-//     const fetchData = async () => {
-//             let query = 'settings/customer_service_phone'
-//             await axios.get(apiUrl + query )
-//             .then((res) => {
-//               setData(res.data.value);
-//             })
-//             .catch(function (error) {
-//                 if(error.response.status === 401){
-//                 localStorage.removeItem('access_token')
-//                 // window.location.reload()
-//                 }
-//             });
-//     };
-
-//     useEffect(() => {
-//       fetchData();
-//     }, []);
-
-
     return(
         <div>
             <div className="bg-footer-comp">
                     <div className="container max-auto">
-                        <div className="c-footer-content flex flex-wrap">
+                        {/* <div className="c-footer-content flex flex-wrap">
                                 <div className="c-footer__logo w-full mb-14">
                                         <img src={logoWhite} className="footer-logo-white"/>
                                 </div>
-                        </div>
+                        </div> */}
                         <div className="c-footer__top w-full xl:w-9/12 flex flex-wrap">
                                 <div className="c-footer__company w-1/2 md:w-1/4">
-                                        <p className="c-footer__title  md:mb-8 lg:mb-6">Kontak Kami</p>
-                                        <ul className="space-y-4 md:space-y-6 lg:space-y-4">
-                                            <li className="gtm-footer-email gtm-footer"><a className="c-footer__link"  href="mailto:balisanti.siwalatri@gmail.com" target="_blank"  ><span> balisanti.siwalatri@gmail.com</span></a></li>
-                                            {/* <li className="gtm-footer-phone gtm-footer"><a className="c-footer__link"  href={"http://wa.me/"} target="_blank"><Whatsapp /><span> +{datas.customer}</span></a></li>
-                                            <li className="gtm-footer-address gtm-footer"><a className="c-footer__link" href="https://goo.gl/maps/eAoH9vf5Pmeyfojn8" target="_blank" ><span> Jl. Raya Buduk, No. 9x, Mengwi,<br/> Badung, Bali</span></a></li> */}
-                                        </ul>
-                                </div>
-                                {/* <div className="c-footer__company w-1/2 md:w-1/4">
-                                        <p className="c-footer__title  md:mb-8 lg:mb-6">Tentang Kami</p>
-                                        <ul className="space-y-4 md:space-y-6 lg:space-y-4">
-                                            <li className="gtm-footer-email"><a href="/profile-company" className="c-footer__link">Yuk Kenali Kami</a></li>
-                                            
-                                        </ul>
+                                        <img src={logoWhite} className="footer-logo-white"/>
                                 </div>
                                 <div className="c-footer__company w-1/2 md:w-1/4">
-                                        <p className="c-footer__title  md:mb-8 lg:mb-6">Partner</p>
-                                        <ul className="space-y-4 md:space-y-6 lg:space-y-4">
+                                        {/* <p className="c-footer__title  md:mb-8 lg:mb-6">Partner</p>
+                                        <ul>
                                             <li className="gtm-footer-email"><a href="/partners/driver" className="c-footer__link">Driver</a></li>
                                             <li className="gtm-footer-phone"><a href="/partners/merchant" className="c-footer__link">Mitra Usaha</a></li>
-                                        </ul>
+                                        </ul> */}
                                 </div>
                                 <div className="c-footer__company w-1/2 md:w-1/4">
-                                        <p className="c-footer__title  md:mb-8 lg:mb-6">Unduh Aplikasi</p>
-                                        <ul className="space-y-4 md:space-y-6 lg:space-y-4">
-                                            <li className="gtm-footer-email gtm-footer"><a href="https://play.google.com/store/apps/details?id=com.bagocustomer" target="_blank"  className="c-footer__link"><FontAwesomeIcon icon={faGooglePlay} />  Google Play</a></li>
-                                            <li className="gtm-footer-phone gtm-footer"><a href="https://apps.apple.com/id/app/bago/id1598782885" target="_blank"  className="c-footer__link"><FontAwesomeIcon icon={faApple} /> Apps Store</a> </li>
+                                        <p className="c-footer__title  md:mb-8 lg:mb-6">Butuh Bantuan?</p>
+                                        <ul>
+                                            <li className="gtm-footer-email gtm-footer"><a href="/contact-us" target="_blank"  className="c-footer__link">Kontak Kami</a></li>
                                         </ul>
-                                </div> */}
+                                        {/* <div class="input-group mb-3">
+                                        <input type="text" class="form-control" placeholder="Recipient's username" aria-label="Recipient's username" aria-describedby="button-addon2" />
+                                        <div class="input-group-append">
+                                        <button class="btn btn-outline-secondary" type="button" id="button-addon2">Button</button>
+                                        </div>
+                                        </div> */}
+                                </div>
+                                <div className="c-footer__company w-1/2 md:w-1/4">
+                                        <p className="c-footer__title md:mb-8 lg:mb-6">Tentang Kami</p>
+                                        <p className="c-footer__info md:mb-8 lg:mb-6">Bali Santi menyediakan layanan pemesanan tiket penyebrangan domestik di Klungkung dengan berbagai fitur seperti pembayaran fleksibel, pengaturan pemesanan yang mudah, dan pengiriman tiket langsung melalui e-mail.</p>
+                                </div>
                         </div>
-                        {/* <hr className="xl" />
+                        <hr className="xl" />
                         <div className="row">
                             <div className="c-footer__info w-full  col-xs-6 col-sm-6 col-md-6 col-lg-6">
-                                    <div className="c-footer__privpol gtm-footer-privpol"><a href="/privacy" className="c-footer__link">Kebijakan Privasi</a></div>
-                                    <div className="c-footer__tnc gtm-footer-tnc"><a href="/layanan" className="c-footer__link">Ketetuan Layanan</a></div>
+                                    <div className="c-footer__privpol gtm-footer-privpol"><a>2024 © Dinas Perhubungan Kabupaten Klungkung</a></div>
+                                    {/* <div className="c-footer__tnc gtm-footer-tnc"><a href="/layanan" className="c-footer__link">Ketetuan Layanan</a></div> */}
                             </div>
                             <div className="c-footer__info_social w-full col-xs-6 col-sm-6 col-md-6 col-lg-6">
-                                    <div className="c-footer__privpol gtm-footer-privpol"><a href="https://vt.tiktok.com/ZGJUjkeKh/" target="_blank"  className="btn c-footer__link"><FontAwesomeIcon icon={faTiktok} className="c-footer__icon" /></a></div>
-                                    <div className="c-footer__privpol gtm-footer-privpol"><a href="https://www.linkedin.com/in/bago-bali-2a4101208" target="_blank"  className="btn c-footer__link"><Linkedin className="c-footer__icon" /></a></div>
-                                    <div className="c-footer__privpol gtm-footer-privpol"><a href="https://www.facebook.com/bago.bali.1" target="_blank"  className="btn c-footer__link"><Facebook className="c-footer__icon"/></a></div>
-                                    <div className="c-footer__privpol gtm-footer-privpol"><a href={"http://wa.me/"+datas.customer} target="_blank"  className="btn c-footer__link"><Whatsapp className="c-footer__icon"/></a></div>
-                                    <div className="c-footer__privpol gtm-footer-privpol"><a href="https://www.instagram.com/bago_bali?r=nametag" target="_blank"  className="btn c-footer__link"><Instagram className="c-footer__icon"/></a></div>
-                                    <div className="c-footer__privpol gtm-footer-privpol"><a href="https://www.youtube.com/channel/UCjzMdfQUhHTGHtE-OQ3y2dw" target="_blank"  className="btn c-footer__link"><Youtube className="c-footer__icon" /></a></div>
-
+                                <div className="c-footer__privpol gtm-footer-privpol"><a href="https://dishub.klungkungkab.go.id" target="_blank"  className="btn c-footer__link"><FontAwesomeIcon icon={faGlobe} className="c-footer__icon" /></a></div>
+                                <div className="c-footer__privpol gtm-footer-privpol"><a href="https://www.facebook.com/p/Dinas-Perhubungan-Kabupaten-Klungkung-100014923306880/" target="_blank"  className="btn c-footer__link"><FontAwesomeIcon icon={faFacebook} className="c-footer__icon"/></a></div>
+                                <div className="c-footer__privpol gtm-footer-privpol"><a href="https://www.instagram.com/dishubklungkung/" target="_blank"  className="btn c-footer__link"><FontAwesomeIcon icon={faInstagram} className="c-footer__icon"/></a></div>
+                                <div className="c-footer__privpol gtm-footer-privpol"><a href="mailto:balisanti.siwalatri@gmail.com" target="_blank"  className="btn c-footer__link"><FontAwesomeIcon icon={faEnvelope} className="c-footer__icon" /></a></div>
                             </div>
-                        </div> */}
+                        </div>
         </div>
       </div>
-      <div className="powerded-bg-comp">
+      {/* <div className="powerded-bg-comp">
         <div className="container max-auto">
           <div className="c-footer-content flex flex-wrap">
             <h5>Powered by : DisHub Klungkung x BPD Bali x MaiHarta</h5>
           </div>
         </div>
-      </div>
+      </div> */}
     </div>
   );
 };
